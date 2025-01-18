2583418dabfc123f7a255df05363069c86323948
fil-low <fmalavolta2007@gmail.com>
2025-01-18 18:50:19 +0100

    nuovi membri team

--- src/app/website/pages/index/index.component.ts
+++ src/app/website/pages/index/index.component.ts
@@ -10,20 +10,14 @@ import Flickity from 'flickity';
 export class IndexComponent {
   private flickityInstance: Flickity | undefined;
   components = [
-    {"name": "Pietro", "link": "https://www.instagram.com/pietro.peerani/", "color": "#FF5E5B", "image": "pietro.svg"},
-    {"name": "Alessia", "color": "#FFED66", "image": "alessia.svg"},
-    {"name": "Thomas", "color": "#00CECB", "image": "pietro.svg"},
-    {"name": "Alice", "color": "#FFFFEA", "image": "alessia.svg"},
-    {"name": "Andrea", "color": "#61988E", "image": "pietro.svg"},
-    {"name": "Eleonora", "color": "#EABDA8", "image": "alessia.svg"},
-    {"name": "Lorenzo", "color": "#D36135", "image": "pietro.svg"},
-    {"name": "Matteo", "color": "#E6E49F", "image": "pietro.svg"},
+    {"name": "Andrea", "color": "#FFED66", "image": "pietro.svg"},
     {"name": "Filippo", "link": "https://www.instagram.com/fillo.mv/","color": "#84A98C", "image": "pietro.svg"},
+    {"name": "Mia", "color": "#00CECB", "image": "alessia.svg"},
+    {"name": "Alessandro", "link": "https://www.instagram.com/alessandro.sabbadinii/","color": "#FFFFEA", "image": "pietro.svg"},
+    {"name": "Martin", "color": "#61988E", "image": "pietro.svg"},
+    {"name": "Eleonora", "link": "https://www.instagram.com/e.leonoraverri/","color": "#EABDA8", "image": "alessia.svg"},
+    {"name": "Francesco", "link": "https://www.instagram.com/fra.cinque.0_0/", "color": "#E6E49F", "image": "pietro.svg"},
     {"name": "Giuseppe", "color": "#F1AB86", "image": "pietro.svg"},
-    {"name": "Neri", "color": "#F7DBA7", "image": "pietro.svg"},
-    {"name": "Andrea", "link": "https://www.instagram.com/andre.delgiu/", "color": "#DBD9DB", "image": "alessia.svg"},
-    {"name": "Alessandro", "link": "https://www.instagram.com/aim.alle_/", "color": "#E7EFC5", "image": "pietro.svg"},
-    {"name": "Ludovica", "link": "https://www.instagram.com/ludovica.valentee/", "color": "#F896D8", "image": "alessia.svg"},
   ]
 
   constructor(private el: ElementRef) {}
